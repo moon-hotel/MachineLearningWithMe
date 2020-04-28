f83ffdaac861c5592870c8aa0cc2534501057983
moon-hotel <moon-hotel@hotmail.com>
2020-04-28 17:25:32 +0800

    线性回归第二次推送

---
+++ 01_LinearRegression/201_train.py
@@ -0,0 +1,25 @@
+from sklearn.datasets import load_boston
+from sklearn.linear_model import LinearRegression
+
+
+def load_data():
+    data = load_boston()
+    x = data.data
+    y = data.target
+    return x, y
+
+
+def train(x, y):
+    print('X的形状：',x.shape)
+    model = LinearRegression()
+    model.fit(x,y)
+    print("权重为：",model.coef_)
+    print("偏置为：",model.intercept_)
+    print("第12个房屋的预测价格：",model.predict(x[12,:].reshape(1,-1)))
+    print("第12个房屋的真实价格：",y[12])
+
+
+if __name__ == '__main__':
+    x,y = load_data()
+    train(x,y)
+
--- 01_LinearRegression/README.md
+++ 01_LinearRegression/README.md
@@ -1,6 +1,6 @@
 # 线性回归
 
-## 1. 第一次推送
+## 1 第一次推送
 
 ### 1.1 内容
 
@@ -10,6 +10,20 @@
 
 - 安装好对应库并完成示例代码的运行
   - [示例1](./101_visualization.py)，[示例2](./102_train.py)
+
+## 2. 第二次推送
+
+### 2.1 内容
+
+- [线性回归（多变量与多项式回归）]()
+
+### 2.2 练习
+
+- 完成示例代码的运行
+  - [示例1](./201_train.py)，[示例2](./202_train_pol.py)
+- 浏览sklearn官方文档
+  - [sklearn API（官方）](https://scikit-learn.org/stable/modules/classes.html)
+  - [sklearn API （中文社区）](https://sklearn.apachecn.org/)
 
 
 
