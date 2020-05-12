384bf73e9bb618b2509690b47bd22977c848a46a
moon-hotel <moon-hotel@hotmail.com>
2020-05-12 21:22:03 +0800

    模型的改善第二次推送

---
+++ 03_ModelOptimization/302_visualization_overgitting_cla.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.datasets import make_circles
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+
+def standarlization(X):
+    mean = X.mean(axis=0)
+    std = X.std(axis=0)
+    return (X - mean) / std
+
+
+def make_nonlinear_cla_data():
+    num_points = 200
+    x, y = make_circles(num_points, factor=0.5, noise=0.17, random_state=np.random.seed(10))
+    x = x.reshape(-1, 2)
+    x = standarlization(x)
+    return x, y.reshape(-1, 1)
+
+
+def decision_boundary(x, y, pol):
+    model = LogisticRegression()
+    model.fit(x, y)
+    print("Accuracy:", model.score(x, y))
+    x_min, x_max = x[:, 0].min() - .5, x[:, 0].max() + .5
+    y_min, y_max = x[:, 1].min() - .5, x[:, 1].max() + .5
+    h = .01  # step size in the mesh
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    x_new = np.hstack([xx.reshape(-1, 1), yy.reshape(-1, 1)])
+    Z = model.predict(pol.transform(x_new))
+
+    Z = Z.reshape(xx.shape)
+    plt.figure(1, figsize=(6, 5))
+    plt.pcolormesh(xx, yy, Z, cmap=plt.cm.Paired)
+    plt.scatter(x[:, 0], x[:, 1], c=y, alpha=0.83)  # alpha 控制透明度
+    plt.show()
+
+
+if __name__ == '__main__':
+    x, y = make_nonlinear_cla_data()
+    pol = PolynomialFeatures(degree=15, include_bias=False)
+    x_pol = pol.fit_transform(x)
+
+    decision_boundary(x_pol, y, pol)
--- 03_ModelOptimization/README.md
+++ 03_ModelOptimization/README.md
@@ -12,3 +12,14 @@
 - 完成示例代码的运行并理解
   - [示例1](./101_visualization_projection.py)，[示例2](102_visualization_contour.py)，[示例3](./103_visualization_gradietn.py)，[示例4](./104_standarlization_reg.py)，[示例5](./105_standarlization_cla.py)
   - [示例6](./106_visualization_nonlinear.py)，[示例7](./107_visualization_pol_reg.py)，[示例8](./108_visualization_pol_cla.py)，[示例9](./201_visualization_grad_contour.py)，[示例10](./202_visualization_grad.py)
+
+## 2 第二次推送
+
+### 2.1 内容
+
+- [模型的改善与泛化（过拟合）](https://mp.weixin.qq.com/s/uF2Zp90FayUu-YqHUcKuGw)
+
+### 2.2 练习
+
+- 完成示例代码的运行并理解
+  - [示例1](./301_visualization_overfitting_reg.py)，[示例2](./302_visualization_overgitting_cla.py)
