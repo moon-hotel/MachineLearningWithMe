8be60b72a833873dfa74fac14113b3fd54c31ba7
moon-hotel <moon-hotel@hotmail.com>
2021-07-24 09:46:49 +0800

    添加KNN原理示例图

---
+++ AllBooKCode/Chapter05/01_visualzation.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.datasets import make_blobs
+
+
+def visualization():
+    num_points = 300
+    plt.figure(figsize=(6, 6), dpi=120)
+    centers = [[1, 1], [1.8, 1.6], [1.8, 0.7]]  # 指定中心
+    new_point = [1.55, 1.15]
+    plt.scatter(new_point[0], new_point[1], s=4200, edgecolors='black', linewidths=1.5, c='white')
+    x, y = make_blobs(n_samples=num_points, centers=centers,
+                      cluster_std=0.2, random_state=np.random.seed(3))
+    index_c0, index_c1, index_c2 = (y == 0), (y == 1), (y == 2)
+    c0, c1, c2 = x[index_c0], x[index_c1], x[index_c2]
+    plt.scatter(c0[:, 0], c0[:, 1], marker='s', label='class 0', s=50)
+    plt.scatter(c1[:, 0], c1[:, 1], marker='o', label='class 1', s=50)
+    plt.scatter(c2[:, 0], c2[:, 1], marker='*', label='class 2', s=50)
+    plt.scatter(new_point[0], new_point[1], marker='v', s=60, c='black')
+    plt.annotate("Who am i ? which class do I belong to ?", xy=(1.56, 1.15), fontsize=13,
+                 xytext=(0.4, 1.5), arrowprops=dict(arrowstyle="<-", connectionstyle="arc3"))
+    plt.xticks([])
+    plt.yticks([])
+    # plt.rcParams['font.sans-serif'] = ['SimSun']  # 用来  正常显示中文标签
+    plt.legend(fontsize=15)
+    plt.tight_layout()  # 调整子图间距
+    plt.show()
+
+
+if __name__ == '__main__':
+    visualization()
