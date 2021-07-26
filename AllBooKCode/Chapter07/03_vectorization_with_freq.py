6f968e2bd4b161616044dba7ad72d3517ed33696
moon-hotel <moon-hotel@hotmail.com>
2021-07-26 19:01:43 +0800

    添加词袋模型示例代码

---
+++ AllBooKCode/Chapter07/03_vectorization_with_freq.py
@@ -0,0 +1,37 @@
+import jieba
+from collections import Counter
+
+
+def vectorization_with_freq(s):
+    cut_words = ""
+    x_text = []
+    #---------分词处理------------------
+    for item in s:
+        seg_list = jieba.cut(item, cut_all=False)
+        tmp = " ".join(seg_list)
+        cut_words += (tmp + " ")
+        x_text.append(tmp.split())
+    all_words = cut_words.split()
+    c = Counter()
+    for x in all_words:
+        if len(x) > 1 and x != '\r\n':
+            c[x] += 1
+    # ---------词频统计构造词表------------------
+    vocab = []
+    for (k, v) in c.most_common(8):  # 输出词频最高的前8个词
+        vocab.append(k)
+    # ---------向量化------------------
+    x_vec = []
+    for item in x_text:
+        tmp = dict(zip(vocab,[0]*len(vocab)))
+        for w in item:
+            if w in vocab:
+                tmp[w] += 1
+        x_vec.append(list(tmp.values()))
+    print("词表：",vocab)
+    print("文本：",x_text)
+    print(x_vec)
+
+if __name__ == '__main__':
+    s = ['文本分词工具可用于对文本进行分词处理', '常见的用于处理文本的分词处理工具有很多']
+    vectorization_with_freq(s)
