ab1cd747b6fc0b5577b7fb88ed9bcad0168267b6
moon-hotel <moon-hotel@hotmail.com>
2021-07-26 19:02:49 +0800

    添加tf-idf计算示例代码

---
+++ AllBooKCode/Chapter07/06_tf_idf.py
@@ -0,0 +1,17 @@
+corpus = ['this is the first document',
+          'this document is the second document',
+          'and this is the third one',
+          'is this the first document']
+vocabulary = ['this', 'document', 'first', 'is', 'second', 'the',
+              'and', 'one']
+
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+
+count = CountVectorizer(vocabulary=vocabulary)
+count_matrix = count.fit_transform(corpus).toarray()
+tfidf_trans = TfidfTransformer(norm=None)
+tfidf_matrix = tfidf_trans.fit_transform(count_matrix)
+idf_vec = tfidf_trans.idf_
+
+print(tfidf_matrix.toarray())
