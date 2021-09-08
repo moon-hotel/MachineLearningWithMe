feb40e68b4ee8d6da851d8b29dc94341c3420fe3
moon-hotel <moon-hotel@hotmail.com>
2021-09-08 19:25:55 +0800

    添加k-means思想示意图

---
+++ AllBooKCode/Chapter10/01_kmeans_visualization_cluster.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def visualization():
+    centers = [[2.2, 1], [3.8, 1], [3, 2.8]]  # 指定簇中心
+    x, y = make_blobs(n_samples=800, centers=centers, cluster_std=0.3, random_state=200)
+    markers = ['o', 's', 'v']
+    for i in range(len(centers)):
+        index = np.where(y == i)[0]
+        cluster = x[index, :]
+        plt.scatter(cluster[:, 0], cluster[:, 1], s=60, marker=markers[i], cmap=plt.cm.Paired)
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+
+
+if __name__ == '__main__':
+    visualization()
