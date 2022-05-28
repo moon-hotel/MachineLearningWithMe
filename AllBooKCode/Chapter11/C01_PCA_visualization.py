45dd9e49e72cb96a61e8ce6b0385e8fcd2d50a0f
moon-hotel <moon-hotel@hotmail.com>
2022-05-28 20:58:36 +0800

    添加可视化示例

--- AllBooKCode/Chapter11/C01_PCA_visualization.py
+++ AllBooKCode/Chapter11/C01_PCA_visualization.py
@@ -4,7 +4,54 @@ from sklearn.decomposition import PCA
 from mpl_toolkits.mplot3d import Axes3D
 
 
-def make_data_visualization():
+def visualization1():
+    rng = np.random.RandomState(10)
+    n_samples = 20
+    cov = [[3, 3], [3, 4]]
+    X = rng.multivariate_normal(mean=[0, 0], cov=cov, size=n_samples)  # 构造一个二元正态分布数据集
+    plt.figure(figsize=(12, 4))
+
+    plt.subplot(1, 3, 1)
+    plt.scatter(X[:, 0], X[:, 1], alpha=1., c='black')
+    plt.xlim(-3.5, 3.5)
+    plt.ylim(-3.5, 3.5)
+    plt.xlabel(r"$x$")
+    plt.ylabel(r"$y$")
+
+    plt.subplot(1, 3, 2)
+    pca = PCA(n_components=2).fit(X)
+    plt.scatter(X[:, 0], X[:, 1], alpha=1., c='black')
+    comps = pca.components_
+    comp = comps[0] * 4.5
+    plt.plot([comp[0], -comp[0]], [comp[1], -comp[1]], c='r', label=r"$z_1$")
+    comp = comps[1] * 1.5
+    plt.plot([comp[0], -comp[0]], [comp[1], -comp[1]], c='r', linestyle='--', label=r"$z_2$")
+    plt.xlim(-3.5, 3.5)
+    plt.ylim(-3.5, 3.5)
+    plt.xlabel(r"$x$")
+    plt.ylabel(r"$y$")
+    plt.gca().set(aspect="equal")  # x,y轴按等比例进行展示
+    plt.legend()
+
+    plt.subplot(1, 3, 3)
+    pca = PCA(n_components=1).fit(X)
+    X_new = pca.transform(X)
+    plt.scatter(X_new.ravel(), [-3.4] * len(X_new), c='black')
+    a = plt.gca()
+    # a.spines['right'].set_visible(False)
+    # a.spines['top'].set_visible(False)
+    # a.spines['bottom'].set_position('center')
+    # a.spines['left'].set_position('center')
+    plt.xlabel(f"$z$")
+    plt.xlim(-3.5, 3.5)
+    plt.ylim(-3.5, 3.5)
+    plt.yticks([])
+
+    plt.tight_layout()
+    plt.show()
+
+
+def visualization2():
     rng = np.random.RandomState(0)
     n_samples = 300
     cov = [[3, 3], [3, 4]]
@@ -33,7 +80,7 @@ def eigenvalue_vs_singular():
     cov = [[3, 3], [3, 4]]
     X = rng.multivariate_normal(mean=[0, 0], cov=cov, size=n_samples)  # 构造一个二元正态分布数据集
 
-    plt.figure(figsize=(6, 4))
+    plt.figure(figsize=(12, 4))
     plt.subplot(1, 2, 1)
     pca = PCA(n_components=2).fit(X)
     plt.scatter(X[:, 0], X[:, 1], alpha=0.5, label="samples")
@@ -102,6 +149,7 @@ def visualization_3d():
 
 
 if __name__ == '__main__':
-    make_data_visualization()
+    visualization1()
+    visualization2()
     visualization_3d()
     eigenvalue_vs_singular()
