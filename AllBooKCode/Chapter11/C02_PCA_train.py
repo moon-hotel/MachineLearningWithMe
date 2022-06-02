806b8e960d8b8f133f5845991de3fd12dc4472d7
moon-hotel <moon-hotel@hotmail.com>
2022-06-02 22:51:56 +0800

    添加PCA示例代码与索引

---
+++ AllBooKCode/Chapter11/C02_PCA_train.py
@@ -0,0 +1,34 @@
+from sklearn.decomposition import PCA
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+
+
+def load_data(reduction=False, n_components=2):
+    x, y = load_iris(return_X_y=True)
+    x_train, x_test, y_train, y_test = \
+        train_test_split(x, y, test_size=0.3, random_state=2022)
+    if reduction:
+        pca = PCA(n_components)
+        x_train = pca.fit_transform(x_train)
+        x_test = pca.transform(x_test)
+    return x_train, x_test, y_train, y_test
+
+
+def decision_tree():
+    x_train, x_test, y_train, y_test = load_data(reduction=False)
+    model = DecisionTreeClassifier()
+    model.fit(x_train, y_train)
+    acc = model.score(x_test, y_test)
+    print(f"降维前的准确率为：{acc}，形状为{x_train.shape}")
+
+    x_train, x_test, y_train, y_test = load_data(reduction=True)
+    model = DecisionTreeClassifier()
+    model.fit(x_train, y_train)
+    acc = model.score(x_test, y_test)
+    print(f"降维后的准确率为：{acc}，形状为{x_train.shape}")
+
+
+
+if __name__ == '__main__':
+    decision_tree()
---
+++ AllBooKCode/Chapter11/README.md
@@ -0,0 +1,8 @@
+## 代码索引
+
+### 主成分分析原理与实现
+
+- [PCA可视化 代码](C01_PCA_visualization.py)
+- [sklearn中PCA使用示例 代码](C02_PCA_train.py)
+
+### [返回主页](../../README.md)&nbsp;&nbsp;&nbsp;&nbsp;[返回索引](../Index.md)
