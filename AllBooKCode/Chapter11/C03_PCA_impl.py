494cd66481e90c0bd4796a0be4eaf0bd40eae6ad
moon-hotel <moon-hotel@hotmail.com>
2022-06-05 09:15:56 +0800

    添加特征标准化

--- AllBooKCode/Chapter11/C03_PCA_impl.py
+++ AllBooKCode/Chapter11/C03_PCA_impl.py
@@ -3,6 +3,7 @@ from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 
 class MyPCA(object):
@@ -10,6 +11,8 @@ class MyPCA(object):
         self.n_components = n_components
 
     def fit(self, X):
+        self.standar_scaler = StandardScaler()
+        X = self.standar_scaler.fit_transform(X)
         # 计算特征值和特征向量
         w, v = np.linalg.eig(np.matmul(X.T, X) / len(X))
         # v[:,i] 是特征值w[i]所对应的特征向量
@@ -24,10 +27,11 @@ class MyPCA(object):
 
     def transform(self, X):
         # 降维
+        X = self.standar_scaler.transform(X)
         return np.matmul(X, self.v)  # [m,n] @ [n,k] = [m,k]
 
 
-def load_data(reduction_method='my_pca', n_components=2):
+def load_data(reduction_method='my_pca', n_components=3):
     x, y = load_iris(return_X_y=True)
     x_train, x_test, y_train, y_test = \
         train_test_split(x, y, test_size=0.3, random_state=2022)
@@ -42,19 +46,19 @@ def load_data(reduction_method='my_pca', n_components=2):
 
 def test_PCA():
     x_train, x_test, y_train, y_test = load_data(reduction_method="my_pca",
-                                                 n_components=2)
+                                                 n_components=3)
 
     model = DecisionTreeClassifier()
     model.fit(x_train, y_train)
     acc = model.score(x_test, y_test)
-    print(f"降维前的准确率为：{acc}，形状为{x_train.shape}")
+    print(f"MyPCA降维后的准确率为：{acc}，形状为{x_train.shape}")
 
     x_train, x_test, y_train, y_test = load_data(reduction_method="sklearn",
-                                                 n_components=2)
+                                                 n_components=3)
     model = DecisionTreeClassifier()
     model.fit(x_train, y_train)
     acc = model.score(x_test, y_test)
-    print(f"降维后的准确率为：{acc}，形状为{x_train.shape}")
+    print(f"PCA降维后的准确率为：{acc}，形状为{x_train.shape}")
 
 
 if __name__ == '__main__':
