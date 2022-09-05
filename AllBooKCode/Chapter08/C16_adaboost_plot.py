d4b99c13bd04f954399a1f1cd7e8848fc6a4aac6
moon-hotel <moon-hotel@hotmail.com>
2022-09-05 20:22:02 +0800

    添加adaboost权重误差关系图

---
+++ AllBooKCode/Chapter08/C16_adaboost_plot.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def f(x):
+    return np.log((1 - x) / x)
+
+
+if __name__ == '__main__':
+    x = np.linspace(0, 1, 200)
+    plt.plot(x, f(x))
+    a = plt.gca()
+    a.spines['right'].set_visible(False)
+    a.spines['top'].set_visible(False)
+    a.spines['left'].set_position('center')
+    plt.xlabel(r"$err^{(m)}$", fontsize=14)
+    plt.ylabel(r"$\alpha^{(m)}$", fontsize=14)
+    plt.show()
