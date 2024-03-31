0c8a417200551ecadcd89882d4f3a2e30cc5e117
nulls8 <moon-hotel@hotmail.com>
2024-03-31 22:43:24 +0800

    修订代码，改为彩色

--- AllBooKCode/Chapter03/03_one_vs_all_visualization.py
+++ AllBooKCode/Chapter03/03_one_vs_all_visualization.py
@@ -13,9 +13,9 @@ def visualization():
     plt.rcParams['ytick.direction'] = 'in'  # 刻度向内
     plt.rcParams['xtick.direction'] = 'in'  # 刻度向内
     plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
-    plt.scatter(c0[:, 0], c0[:, 1], marker='s', c='black', label='类别1', s=50)
-    plt.scatter(c1[:, 0], c1[:, 1], marker='x', c='black', label='类别2', s=50)
-    plt.scatter(c2[:, 0], c2[:, 1], marker='v', c='black', label='类别3', s=50)
+    plt.scatter(c0[:, 0], c0[:, 1], marker='s', label='类别0', s=50)
+    plt.scatter(c1[:, 0], c1[:, 1], marker='x', label='类别1', s=50)
+    plt.scatter(c2[:, 0], c2[:, 1], marker='v', label='类别2', s=50)
     # plt.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体
     plt.legend(fontsize=15)
     plt.tight_layout()
@@ -34,9 +34,9 @@ def visualization_ova():
     plt.rcParams['ytick.direction'] = 'in'  # 刻度向内
     plt.rcParams['xtick.direction'] = 'in'  # 刻度向内
     plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
-    plt.scatter(c0[:, 0], c0[:, 1], marker='s', c='black', label='类别1', s=50)
-    plt.scatter(c1[:, 0], c1[:, 1], marker='o', c='black', label='类别2', s=50)
-    plt.scatter(c2[:, 0], c2[:, 1], marker='o', c='black', label='类别2', s=50)
+    plt.scatter(c0[:, 0], c0[:, 1], marker='s',  label='类别0', s=50)
+    plt.scatter(c1[:, 0], c1[:, 1], marker='o',  label='类别1', s=50)
+    plt.scatter(c2[:, 0], c2[:, 1], marker='o',  label='类别1', s=50)
     plt.plot([0.65, 2.0], [2.6, 0.85], c='black')
     # plt.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体
     plt.legend(fontsize=14)
@@ -51,9 +51,9 @@ def visualization_ova():
     plt.rcParams['ytick.direction'] = 'in'  # 刻度向内
     plt.rcParams['xtick.direction'] = 'in'  # 刻度向内
     plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
-    plt.scatter(c0[:, 0], c0[:, 1], marker='o', c='black', label='类别2', s=50)
-    plt.scatter(c1[:, 0], c1[:, 1], marker='x', c='black', label='类别1', s=50)
-    plt.scatter(c2[:, 0], c2[:, 1], marker='o', c='black', label='类别2', s=50)
+    plt.scatter(c0[:, 0], c0[:, 1], marker='o',  label='类别1', s=50)
+    plt.scatter(c1[:, 0], c1[:, 1], marker='x',  label='类别0', s=50)
+    plt.scatter(c2[:, 0], c2[:, 1], marker='o',  label='类别1', s=50)
     plt.plot([1.45, 1.55], [0.2, 3.5], c='black')
     # plt.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体
     plt.legend(fontsize=14)
@@ -68,9 +68,9 @@ def visualization_ova():
     plt.rcParams['ytick.direction'] = 'in'  # 刻度向内
     plt.rcParams['xtick.direction'] = 'in'  # 刻度向内
     plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
-    plt.scatter(c0[:, 0], c0[:, 1], marker='o', c='black', label='类别2', s=50)
-    plt.scatter(c1[:, 0], c1[:, 1], marker='o', c='black', label='类别2', s=50)
-    plt.scatter(c2[:, 0], c2[:, 1], marker='v', c='black', label='类别1', s=50)
+    plt.scatter(c0[:, 0], c0[:, 1], marker='o',  label='类别1', s=50)
+    plt.scatter(c1[:, 0], c1[:, 1], marker='o',  label='类别1', s=50)
+    plt.scatter(c2[:, 0], c2[:, 1], marker='v',  label='类别0', s=50)
     plt.plot([0.66, 2.2], [1, 3.1], c='black')
     # plt.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体
     plt.legend(fontsize=14)
