2fdeabc10ba7b361dab52cd8dc4a5b4b128a227a
nulls8 <moon-hotel@hotmail.com>
2025-09-13 16:36:31 +0800

    优化图示代码

--- AllBooKCode/Chapter02/C09_normalization_distribution.py
+++ AllBooKCode/Chapter02/C09_normalization_distribution.py
@@ -8,16 +8,18 @@ def normal_distribution(mu=0, sigma2=2., n=100):
     return x, y
 
 
-
 def visualization_normal_dis():
     x, y = normal_distribution(mu=0, sigma2=1)
     plt.rcParams['xtick.direction'] = 'in'  # 刻度向内
     plt.rcParams['ytick.direction'] = 'in'  # 刻度向内
-    plt.plot(x, y, label=r'$f(x)$',c='black')
+    plt.plot(x, y, label=r'$f(x)$', c='black')
     plt.vlines(0, 0, 0.42, color='black', linestyles='--')
     plt.xlabel("误差", fontsize=15)
-    # plt.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体
-    plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
+    plt.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体
+    plt.rcParams['axes.unicode_minus'] = False
+    # plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
+    plt.tick_params(axis='x', labelsize=14)  # x轴刻度数字大小
+    plt.tick_params(axis='y', labelsize=14)  # y轴刻度数字大小
 
     plt.legend(fontsize=15)
     plt.tight_layout()  # 调整子图间距
