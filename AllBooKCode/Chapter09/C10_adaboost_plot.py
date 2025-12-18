e31dbf4444aabd6960570f320b5d95d99f551e42
nulls9 <moon-hotel@hotmail.com>
2025-12-18 20:49:12 +0800

    添加图示情况

--- AllBooKCode/Chapter09/C10_adaboost_plot.py
+++ AllBooKCode/Chapter09/C10_adaboost_plot.py
@@ -2,17 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def f(x):
-    return np.log((1 - x) / (x + 1e-5))
+def f(x, K=2):
+    return np.log((1 - x) / (x + 1e-5)) + np.log(K - 1)
 
 
 if __name__ == '__main__':
     x = np.linspace(0, 1, 200)
-    plt.plot(x, f(x))
+    plt.plot(x, f(x), label='K=2')
+    plt.plot(x, f(x, 3), label='K=3')
+    plt.plot(x, f(x, 4), label='K=4')
     a = plt.gca()
     a.spines['right'].set_visible(False)
     a.spines['top'].set_visible(False)
     a.spines['left'].set_position('center')
     plt.xlabel(r"$err^{(m)}$", fontsize=14)
     plt.ylabel(r"$\alpha^{(m)}$", fontsize=14)
+    plt.legend(loc='best')
     plt.show()
